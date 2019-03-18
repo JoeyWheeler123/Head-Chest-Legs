761a5319239ad93dee57928de26090f50e463aa7
Ryan-Higgins <c16381886@mydit.ie>
2019-03-18 16:23:43 +0000

    Turns now affected by blocks

---
+++ Head Chest Legs/Assets/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public Text playerOneHealth;
+    public Text playerTwoHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerOneHealth.text = "Health: 5";
+        playerTwoHealth.text = "Health: 5";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
